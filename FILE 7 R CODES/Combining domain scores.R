40cc62438b3f80b37b094b232ca31d0d72622e9b
bimd-project <104343943+bimd-project@users.noreply.github.com>
2022-12-01 13:26:16 +0100

    Update Combining domain scores.R

--- FILE 7 R CODES/Combining domain scores.R
+++ FILE 7 R CODES/Combining domain scores.R
@@ -4,7 +4,7 @@
 # possible to combine the domain scores without the health domain 
 
 # THE BELGIAN INDEX OF MULTIPLE DEPRIVATION 2001 
-bimd2001 <-read.csv("https://raw.githubusercontent.com/bimd-project/Belgian-Indices-of-Multiple-Deprivation/main/FILE%201%20BIMD2001%20DOMAINS%20(SCORE%2C%20RANKS%2C%20DECILES)/bimd2001_domains_score_ranks_deciles.csv")
+bimd2001 <- read.csv("https://raw.githubusercontent.com/bimd-project/Belgian-Indices-of-Multiple-Deprivation/main/FILE%201%20BIMD2001%20DOMAINS%20(SCORE%2C%20RANKS%2C%20DECILES)/BIMD2001_DOMAINS_STATISTICAL_SECTOR_ELLIS_WIDE.csv")
 
 
 bimd2001$BIMD2001_score <- bimd2001$exp_income_score_2001*0.20 + bimd2001$exp_employment_score_2001*0.20 + bimd2001$exp_education_score_2001*0.25 + bimd2001$exp_housing_score_2001*0.15 +
@@ -22,7 +22,7 @@ write.table(bimd2001, "file1", quote = F, sep ="\t", row.names = F, col.names = 
 # THE BELGIAN INDEX OF MULTIPLE DEPRIVATION 2011 
 
 
-bimd2011 <-read.csv("https://raw.githubusercontent.com/bimd-project/Belgian-Indices-of-Multiple-Deprivation/main/FILE%202%20BIMD2011%20DOMAINS%20(SCORES%2C%20RANKS%2C%20DECILES)/bimd2011_domains_score_ranks_deciles.csv")
+bimd2011 <-read.csv("https://raw.githubusercontent.com/bimd-project/Belgian-Indices-of-Multiple-Deprivation/main/FILE%202%20BIMD2011%20DOMAINS%20(SCORES%2C%20RANKS%2C%20DECILES)/BIMD2011_DOMAINS_STATISTICAL_SECTOR_ELLIS_WIDE.csv")
 
 
 bimd2011$BIMD2011_score <- bimd2011$exp_income_score_2011*0.20 + bimd2011$exp_employment_score_2011*0.20 + bimd2011$exp_education_score_2011*0.25 + bimd2011$exp_housing_score_2011*0.15 +
@@ -33,4 +33,3 @@ bimd2011$BIMD2011_rank <- rank(-bimd2011$BIMD2011_score , na.last = 'NA', ties.m
 bimd2011$BIMD2011_deciles <- decile(vector=bimd2011$BIMD2011_rank, decreasing = F) 
 
 write.table(bimd2011, "file", quote = F, sep ="\t", row.names = F, col.names = T)
-
